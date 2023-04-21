903cc1ba46970dcf21d3b5626411720620f20903
Vi Vailati <ambarlife845@gmail.com>
2023-04-20 21:26:44 -0600

    Change env variable to make it available in the frontend

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const getApiUrl = (): string => {
   return process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}/api`
-    : `http://${process.env.API_URL}/api`;
+    : `http://${process.env.NEXT_PUBLIC_API_URL}/api`;
 };
