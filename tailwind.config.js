b5e0b6f63b8a3569c5b28d51659578103c78e1d9
Vi Vailati <ambarlife845@gmail.com>
2023-04-20 20:15:23 -0600

    Reorganize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        fontFamily: {
-            "big-title": ["Lemon_Milk"],
-        },
-        extend: {
-            colors: {
-                "dark-purple": "#865DFF",
-                "dark-pink": "#E384FF",
-                "light-pink": "#FFA3FD",
-                "misty-rose": "#FFEAEA",
-                lion: "#C6A174",
-            },
-        },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      "big-title": ["Lemon_Milk"],
     },
-    plugins: [],
+    extend: {
+      colors: {
+        "dark-purple": "#865DFF",
+        "dark-pink": "#E384FF",
+        "light-pink": "#FFA3FD",
+        "misty-rose": "#FFEAEA",
+        "background-purple": "#191825",
+        lion: "#C6A174",
+      },
+    },
+  },
+  plugins: [],
 };
