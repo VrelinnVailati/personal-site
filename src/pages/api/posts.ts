e426d659475b61d25fa2844439a163a56bba6169
Vi Vailati <ambarlife845@gmail.com>
2023-04-20 21:27:57 -0600

    Add POST action to posts endpoint

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,16 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { getPosts } from "@/data/posts";
+import {createPost, getPosts} from "@/data/posts";
 import { GetPostsResponse } from "@/types/Post";
+import { ResponseOrMessage } from "@/types/Response";
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<GetPostsResponse>
+  res: NextApiResponse<ResponseOrMessage<GetPostsResponse>>
 ) => {
   if (req.method === "GET") {
     const posts = await getPosts();
 
     return res.status(200).json({ posts });
+  }
+
+  if (req.method === "POST") {
+    const { title, shortTag, longTag, sequence, body } = req.body;
+
+    const {id} = await createPost({
+      title,
+      body,
+      sequence,
+      tag: {
+        short: shortTag,
+        long: longTag
+      }
+    })
+
+    return res.status(201).json({ message: "Created post", id });
   }
 };
 
